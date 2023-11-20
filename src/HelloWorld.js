a33118965b2402153c0c0e633da9172b66b5a7b3
emiliapuutonen1 <127953842+emiliapuutonen1@users.noreply.github.com>
2023-11-20 17:16:56 +0200

    muokattu

---
+++ src/HelloWorld.js
@@ -0,0 +1,10 @@
+// HelloWorld.js
+import React from 'react';
+
+const HelloWorld = () => {
+  console.log('HelloWorld component rendered');
+  return <div>Hello, World!</div>;
+};
+
+export default HelloWorld;
+
---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import './MovieCard.css';
+
+export const MovieCard = ({ movie }) => {
+  return (
+    <div className="card-container">
+      <div className="card-img-container">
+        {movie.poster_path && (
+          <img
+            className="card-img"
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            alt="movie-poster"
+          />
+        )}
+      </div>
+      <div className="overlay">
+        <h2>{movie.title}</h2>
+        <p>{movie.overview}</p>
+        <div className="Ratings">
+        <p>Rating: {movie.vote_average}</p>
+        <p>Release Date: {movie.release_date}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+
+
+
+
