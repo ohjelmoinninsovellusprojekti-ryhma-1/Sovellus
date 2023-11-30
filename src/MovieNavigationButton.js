24db6a588e40022588d5451d9ac6162a39590a4e
emiliapuutonen1 <127953842+emiliapuutonen1@users.noreply.github.com>
2023-11-30 20:01:13 +0200

    Lisätty suodatettua hakua elokuville ja sarjoille

---
+++ src/MovieNavigationButton.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import NavigationButton from './navigationnbutton';
+
+
+const MovieNavigationButton = ({ to, label, icon }) => (
+    <NavigationButton to={to} label={label} icon={icon} />
+  
+);
+
+export default MovieNavigationButton;
+
